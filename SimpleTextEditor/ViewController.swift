992b41dc6bf5248634fcbc7574374389ed723963
MedNizar <rockstarhard4@gmail.com>
2021-05-31 16:25:03 +0100

    deleting table view

--- SimpleTextEditor/ViewController.swift
+++ SimpleTextEditor/ViewController.swift
@@ -8,13 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    
+    
+    @IBOutlet weak var result: UILabel!
+    @IBOutlet weak var input: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func emojiAction(_ sender: Any) {
+    }
+    @IBAction func colorSeg(_ sender: UISegmentedControl) {
+        if(sender.selectedSegmentIndex == 0){
+            input.textColor = UIColor.black
 
-
+        }
+        else if(sender.selectedSegmentIndex == 1){
+            input.textColor = UIColor.blue
+        }
+        else{
+            input.textColor = UIColor.red
+        }
+    }
+    @IBAction func boldAction(_ sender: Any) {
+        input.font = UIFont.boldSystemFont(ofSize: 16)
+    }
+    
+    @IBAction func italicAction(_ sender: Any) {
+        input.font = UIFont.boldSystemFont(ofSize: 16)
+    }
+    
+    @IBAction func underlineAction(_ sender: Any) {
+        
+    }
 }
 
